8daa9f131d81310b2a6cead162ee7a7c845bf754
rheiderabe <rheiderline@gmail.com>
2021-02-26 13:40:37 -0300

    add cookies and leveUpModal

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -29,7 +29,7 @@ interface CountdownProviderProps {
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setisActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
   const minutes = Math.floor(time / 60);
@@ -42,7 +42,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setisActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
